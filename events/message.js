a7e539382a409d0eece83aab9ff3cdfe3f1c472b
Kibahor <56401976+Kibahor@users.noreply.github.com>
2021-04-03 13:36:26 +0200

    Utilitaire V8.1

--- events/message.js
+++ events/message.js
@@ -12,18 +12,21 @@ module.exports= (client,Discord,message) =>{
       const command = client.commands.get(commandName)//On récupère la commande en vérifiant le nom et les aliases
     		|| client.commands.find(command => command.aliases && command.aliases.includes(commandName));
 
-      if (!command) return message.channel.send(custom_embed.ToEmbedWarning(`La commande \`${commandName}\` n'existe pas !`));
-
-      if (command.args && !args.length) {
-    		let reply = `Tu n'as fourni aucun arguments !`;
-    		if (command.usage) {reply += `\nSyntax: \`${command.usage}\``;}
-    		return message.channel.send(custom_embed.ToEmbed(reply));
+      if (!command){
+        return message.channel.send(custom_embed.ToEmbedWarning(`La commande \`${commandName}\` n'existe pas !`));
+      }else if(command.permission && !message.member.hasPermission(command.permission)){
+        return message.channel.send(custom_embed.ToEmbedWarning("Vous n'avez pas la permissions d'utiliser cette commande"));
+      }else if (command.args && !args.length) {
+    		let reply;
+    		return message.channel.send(custom_embed.ToEmbed2("Tu n'as fourni aucun arguments !",`\n**Syntax :** \`${command.usage}\``,`Pour plus d'information fait "+help ${command.name}"`));
     	}
+
       try{
-        command.execute(message,args,client);
-      }catch (error){
+        command.execute(message,args,client)
+      }catch(err){
         console.log("\n");
-        console.error(error);
+        console.error(err);
         message.reply(custom_embed.ToEmbedWarning("Une erreur est survenue lors de l'éxecution de la commande"));
       }
+
 }
--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 module.exports = (client,Discord) =>{
-    const eventFiles = fs.readdirSync(`./events`).filter(file => file.endsWith('.js'));
-    for (const file of eventFiles){
-      const event = require(`../events/${file}`);
-      const eventname=file.split('.')[0];
-      client.on(eventname, event.bind(null,client,Discord));
-    }
+
+  const eventFiles = fs.readdirSync(`./events`).filter(file => file.endsWith('.js'));
+  for (const file of eventFiles){
+    const event = require(`../events/${file}`);
+    const eventname=file.split('.')[0];
+    client.on(eventname,event.bind(null,client,Discord));
+  }
 }
